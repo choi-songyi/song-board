3829145fd57ee2bb11fbbe33110a199774830760
choi-songyi <nice.dev.song@gmail.com>
2021-06-14 19:41:57 +0900

    html 대표 타이틀 수정

--- lib/header.php
+++ lib/header.php
@@ -6,7 +6,7 @@ echo '<!DOCTYPE html>
 <meta charset="utf-8">
 <meta http-equiv="Content-Type" content="text/html">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Project 1</title>
+<title>SongBoard</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="../main.css" type="text/css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
